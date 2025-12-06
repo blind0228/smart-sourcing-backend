c98da7f0a23a3dcc4ba240597182a6e5f0270346
JEONG <prophet000228@gmail.com>
2025-12-06 22:28:16 +0900

    feat: add CrossOrigin

--- src/main/java/com/smart/backend/controller/MarketAnalysisController.java
+++ src/main/java/com/smart/backend/controller/MarketAnalysisController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 @RestController
 @RequestMapping("/api/market")
+@CrossOrigin(origins = "*")
 public class MarketAnalysisController {
 
     @Autowired
