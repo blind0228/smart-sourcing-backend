262205a9b2fc0df69b57474d1e2ccbea7625e61d
prophet000228@gmail.com <prophet000228@gmail.com>
2025-12-08 00:33:21 +0900

    feat: /market -> /api/market

--- src/main/java/com/smart/backend/controller/MarketController.java
+++ src/main/java/com/smart/backend/controller/MarketController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/market")
+@RequestMapping("/api/market")
 @RequiredArgsConstructor
 public class MarketController {
 
